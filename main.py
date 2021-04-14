70f8761da6ddb7ca6dcfee69ac5a795e9d49b09a
Shinhye <>
2021-04-14 07:02:01 +0000

    slice practice

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+# slice practice
 inden = "990120-2324343"
-
-print("sex" + inden[7])
-print("year" + inden[0:2])
+print("sex :" + inden[7])
+print("year :" + inden[0:2])
+print("month :" + inden[2:4])
+print("day :" + inden[4:6])
+print("birthday :" +inden[:6])
+print("personal num :" +inden[7:])
+print("personal num :" +inden[-7:]) #from backside
